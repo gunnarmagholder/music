98324fde01dad0e94699a7658564c6984772be67
Gunnar Magholder <gmagholder@mac.com>
2008-10-18 18:28:39 +0200

    Installed stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.home '', :controller => 'root', :action => 'index'
-
+  
   map.resources :users
   map.resource  :session
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.login  '/login', :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  
+  map.connect ':controller/:action/:id'
 end
